2040afc1da34f8af4036089d6240421a21998026
Konstantin Polyzois <konstantin.polyzois@nasdaqomx.com>
2012-10-08 10:00:25 +0200

    Initial commit

---
+++ MinProfileNhibernateLog4Net/NhibernateProfilingAppender.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using StackExchange.Profiling;
+using log4net.Appender;
+using log4net.Core;
+
+namespace MinProfileNhibernateLog4Net
+{
+    public class NhibernateProfilingAppender : AppenderSkeleton
+    {
+        private readonly ThreadLocal<IDisposable> timer = new ThreadLocal<IDisposable>();
+        private string _buildingAnIdbcommandObjectForTheSqlstring = "Building an IDbCommand object for the SqlString";
+
+        public bool Started { get; set; }
+
+
+        public NhibernateProfilingAppender()
+        {
+            Console.WriteLine("hej hopp");
+            Trace.WriteLine("Created");
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            //Sample output of what Nhibernate outputs and this code tries to "parse"
+            //Building an IDbCommand object for the SqlString: INSERT INTO "RecipientList" (Name, Description) VALUES (?, ?); select last_insert_rowid()
+            //Building an IDbCommand object for the SqlString: SELECT this_.Id as Id4_0_, this_.Name as Name4_0_, this_.Description as Descript3_4_0_ FROM "RecipientList" this_
+            if (loggingEvent.LoggerName.Equals("NHibernate.AdoNet.AbstractBatcher"))
+            {
+               
+                string msg = loggingEvent.MessageObject.ToString();
+
+                if (msg.StartsWith(_buildingAnIdbcommandObjectForTheSqlstring))
+                {
+                    MiniProfiler profiler = MiniProfiler.Current;
+                    timer.Value = profiler.Step(logText(msg));
+                }
+                //Sample of NHibernate ouptut when closing 
+                //2011-06-30 10:23:23,515 [9] DEBUG NHibernate.AdoNet.AbstractBatcher - Closed IDbCommand, open IDbCommands: 0
+                if ( msg.StartsWith("Closed IDbCommand"))
+                {
+                    if (timer.Value != null)
+                    {
+                        timer.Value.Dispose();
+
+                        timer.Value = null;
+                    }
+                }
+            }
+
+          
+        }
+
+        /// <summary>
+        /// Remove start "noise" (Building an...) to make a nicer log messgae for MiniProfiler
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public string logText(string msg)
+        {
+            msg = msg.Substring(_buildingAnIdbcommandObjectForTheSqlstring.Length + 2 );
+            if(msg.StartsWith("INSERT"))
+            {
+                return msg.Substring(0,msg.LastIndexOf('"')+1);
+            }
+            if(msg.StartsWith("SELECT"))
+            {
+                return "SELECT " + msg.Substring(msg.IndexOf("FROM"));
+            }
+
+             return msg;
+        }
+
+    }
+}
---
+++ MinProfileNhibernateLog4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MinProfileNhibernateLog4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NASDAQ OMX")]
+[assembly: AssemblyProduct("MinProfileNhibernateLog4Net")]
+[assembly: AssemblyCopyright("Copyright © NASDAQ OMX 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d90b0b9d-51ad-435f-aaf4-b78dfc79dda3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
